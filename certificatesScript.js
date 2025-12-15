79072479b2cb20dac2d6e69daf43b058f6f46459
carl082505 <carlchristianson.gomez@hcdc.edu.ph>
2025-12-15 19:57:13 +0800

    Fix image and script paths for Vercel deployment 2.0

--- certificatesScript.js
+++ certificatesScript.js
@@ -4,9 +4,9 @@ let certAutoScrollInterval;
 
 // Configuration - Add your certificate image paths here
 const certificateList = [
-  '/userAsset/cert1.jpg',
-  '/userAsset/cert3.jpg',
-  '/userAsset/cert4.jpg',
+  './userAsset/cert1.jpg',
+  './userAsset/cert3.jpg',
+  './userAsset/cert4.jpg',
  
 ];
 
